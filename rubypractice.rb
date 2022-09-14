486f47ce418892b0fca1d9999fe2572f1cc247c8
Trina Nixon <TrinaNixon7@gmail.com>
2022-09-13 18:38:25 -0600

    key symbols examples

--- rubypractice.rb
+++ rubypractice.rb
@@ -12,3 +12,16 @@ puts my_bologna[:second_name]
 #ruby hashes, symbols and values
 #in ruby hashes, symbols and their values can be defined in either of 2 ways, using a => or : to separate keys from values 
 
+my_colors = {
+  :color1 => "purple",
+  :color2 => "black"
+  #key symbols and their values can be defined with hash rocket
+  :favorite? => true
+}
+
+my_progress = {
+  program: "Codecademy",
+  language: "Ruby",
+  enthusiastic? = true
+}
+#can also be defined with a colon instead of hash rocket 
