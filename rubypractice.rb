2f96f0219b1f5f40a47cd289e315271b4b9ada5f
Trina Nixon <TrinaNixon7@gmail.com>
2022-09-13 18:34:14 -0600

    ruby symbols

---
+++ rubypractice.rb
@@ -0,0 +1,10 @@
+#ruby symbols
+#symbols are immutable names primarily used as hash keys or for referencing method names
+
+my_bologna = {
+  first_name: "Oscar",
+  second_name: "Meyer",
+  slices: 12
+}
+
+puts my_bologna[:second_name]
