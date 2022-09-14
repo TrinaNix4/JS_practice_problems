64401d829ba22de0ca77fab6f75b5bb97156f193
Trina Nixon <TrinaNixon7@gmail.com>
2022-09-13 18:35:44 -0600

    symbols/values comment

--- rubypractice.rb
+++ rubypractice.rb
@@ -8,3 +8,7 @@ my_bologna = {
 }
 
 puts my_bologna[:second_name]
+
+#ruby hashes, symbols and values
+#in ruby hashes, symbols and their values can be defined in either of 2 ways, using a => or : to separate keys from values 
+
